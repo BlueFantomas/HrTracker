253abc558498b075430bd26be0a01be63152896e
BlueFantomas <bogdan.donisa@yahoo.com>
2015-06-29 15:46:21 +0300

    first commit

---
+++ SourceCode/HrTracker/Ness.HrTracker.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ness.HrTracker.Web.MvcApplication" Language="C#" %>
---
+++ SourceCode/HrTracker/Ness.HrTracker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ness.HrTracker.Web.Startup))]
+namespace Ness.HrTracker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
